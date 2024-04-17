b419f14c7d4c85bda36313f221387345ebe624f6
오은아 <erropp89@naver.com>
2024-04-17 18:01:39 +0900

    [BE] feat: 백엔드 세팅추가 (#1)

---
+++ back/src/main/java/com/example/back/global/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.example.back.global.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("re:code 프로젝트의 API Document")
+                .version("v0.0.1")
+                .description("re:code 프로젝트의 API 명세서입니다.");
+
+        SecurityScheme bearer = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("recode")
+                .bearerFormat("Authorization")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
+
+        Components components = new Components()
+                .addSecuritySchemes("Authorization", bearer);
+
+        return new OpenAPI()
+                .components(components)
+                .addSecurityItem(addSecurityItem)
+                .info(info);
+    }
+}
+
