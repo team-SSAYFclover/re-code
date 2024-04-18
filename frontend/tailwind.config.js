c15c6321624e293c44279367eac79cdf74ce28a8
이수화 <tnghk9611@naver.com>
2024-04-18 16:28:59 +0900

    [FE] chore: tailwind 기본색 지정 (#3)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MAIN1: '#53EDC7',
+        MAIN2: '#ECFFFA',
+        BLACK: '#4B4B4B',
+      },
+    },
   },
   plugins: [],
 };
