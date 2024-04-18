ce4ecdb2d71386769559d13c7044c0b3c2eae4ab
배성연 <amysia@knu.ac.kr>
2024-04-18 17:02:10 +0900

    [FE] feat : 메인화면 컴포넌트 생성(#7)
    -변경 내용 1 : 메인화면 컴포넌트 생성

---
+++ frontend/src/components/home/HomeContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomeContent:React.FC= () => {
+    return (
+        <div>This is Home.</div>
+    )
+}
+
+export default HomeContent
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,5 +1,12 @@
+import HomeContent from "@/components/home/HomeContent";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div>상단바상단바</div>
+      <HomeContent/>
+    </div>
+    );
 };
 
 export default HomePage;
