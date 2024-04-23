601c89f1d28775d77cce0e115a59f25162f4dd36
이수화 <tnghk9611@naver.com>
2024-04-23 17:51:19 +0900

    [FE] fix: 헤더 내 프로필 이미지 수정되도록 수정

--- frontend/src/components/@common/Header.tsx
+++ frontend/src/components/@common/Header.tsx
@@ -1,21 +1,33 @@
 import defaultProfile from '@/assets/default_profile.png';
+import MySettingModal from '@/components/my/MySettingModal';
+import userStore from '@/stores/userStore';
+import { useState } from 'react';
 import { FiBell } from 'react-icons/fi';
 
 const Header = () => {
-  // const { isLogin } = userStore();
-  const isLogin = true;
+  const { isLogin, avatar_url } = userStore();
+
+  const [isShowSetting, setIsShowSetting] = useState<boolean>(false);
 
   return (
-    <div className="w-full h-16 flex justify-end items-center">
-      {isLogin && (
-        <>
-          <span className="cursor-pointer">
-            <FiBell size="25" color="#484848" />
-          </span>
-          <img src={defaultProfile} alt="profile" className="w-8 mx-4 cursor-pointer" />
-        </>
-      )}
-    </div>
+    <>
+      <div className="w-full h-16 flex justify-end items-center">
+        {isLogin && (
+          <>
+            <span className="cursor-pointer">
+              <FiBell size="25" color="#484848" />
+            </span>
+            <img
+              src={avatar_url === '' ? defaultProfile : avatar_url}
+              alt="profile"
+              className="w-8 mx-4 cursor-pointer"
+              onClick={() => setIsShowSetting(!isShowSetting)}
+            />
+          </>
+        )}
+      </div>
+      {isShowSetting && <MySettingModal onClose={() => setIsShowSetting(false)} />}
+    </>
   );
 };
 
