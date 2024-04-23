b84dca2e47f1e8282323a2e13f628e403fe8f085
이수화 <tnghk9611@naver.com>
2024-04-23 17:50:55 +0900

    [FE] refactor: 절대경로로 수정

--- frontend/src/components/@common/Layout.tsx
+++ frontend/src/components/@common/Layout.tsx
@@ -1,6 +1,6 @@
+import Header from '@/components/@common/Header';
+import Navbar from '@/components/@common/Navbar';
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import Navbar from './Navbar';
 
 const Layout = () => {
   return (
