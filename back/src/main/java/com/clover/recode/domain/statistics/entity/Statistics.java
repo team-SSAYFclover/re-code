ccb19550650f2898ae33552c063870216bc5efa0
오은아 <erropp89@naver.com>
2024-04-24 17:25:28 +0900

    [BE] Fix: 예약어 이슈, rank-> ranking으로 변경 (#5)

--- back/src/main/java/com/clover/recode/domain/statistics/entity/Statistics.java
+++ back/src/main/java/com/clover/recode/domain/statistics/entity/Statistics.java
@@ -2,7 +2,9 @@ package com.clover.recode.domain.statistics.entity;
 
 import com.clover.recode.domain.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +22,16 @@ public class Statistics {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private int rank;
+    @NotNull
+    @ColumnDefault("100")
+    private int ranking;
 
     private int supplementaryNo;
 
     private int randomNo;
 
+    @NotNull
+    @ColumnDefault("0")
     private int sequence;
 
     @OneToOne
