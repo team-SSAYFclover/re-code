2fb13bf29335122d76821d59b0637b13e527a30d
이수화 <tnghk9611@naver.com>
2024-04-24 16:46:58 +0900

    [FE] fix: 동시에 2개의 모달이 안열리도록 수정

--- frontend/src/components/@common/Header.tsx
+++ frontend/src/components/@common/Header.tsx
@@ -3,30 +3,49 @@ import MySettingModal from '@/components/my/MySettingModal';
 import userStore from '@/stores/userStore';
 import { useState } from 'react';
 import { FiBell } from 'react-icons/fi';
+import AlarmModal from '../alarm/AlarmModal';
 
 const Header = () => {
   const { isLogin, avatar_url } = userStore();
 
   const [isShowSetting, setIsShowSetting] = useState<boolean>(false);
+  const [isShowAlarm, setIsShowAlarm] = useState<boolean>(false);
+
+  const handleOpen = (type: 'setting' | 'alarm') => {
+    if (type === 'setting') {
+      if (isShowAlarm) {
+        setIsShowAlarm(false);
+      }
+
+      setIsShowSetting(!isShowSetting);
+    } else {
+      if (isShowSetting) {
+        setIsShowSetting(false);
+      }
+
+      setIsShowAlarm(!isShowAlarm);
+    }
+  };
 
   return (
     <>
       <div className="w-full h-16 flex justify-end items-center">
         {isLogin && (
           <>
-            <span className="cursor-pointer">
+            <span className="cursor-pointer z-10" onClick={() => handleOpen('alarm')}>
               <FiBell size="25" color="#484848" />
             </span>
             <img
               src={avatar_url === '' ? defaultProfile : avatar_url}
               alt="profile"
-              className="w-8 mx-4 cursor-pointer"
-              onClick={() => setIsShowSetting(!isShowSetting)}
+              className="w-8 mx-4 cursor-pointer z-10"
+              onClick={() => handleOpen('setting')}
             />
           </>
         )}
       </div>
       {isShowSetting && <MySettingModal onClose={() => setIsShowSetting(false)} />}
+      {isShowAlarm && <AlarmModal onClose={() => setIsShowAlarm(false)} />}
     </>
   );
 };
