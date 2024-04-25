c6b154b4b11691429324ce8100193b5c809c5e7c
오은아 <erropp89@naver.com>
2024-04-25 17:58:42 +0900

    [BE] Fix: id 타입 Long으로 변경(#23)

--- back/src/main/java/com/clover/recode/domain/statistics/entity/Statistics.java
+++ back/src/main/java/com/clover/recode/domain/statistics/entity/Statistics.java
@@ -20,7 +20,7 @@ public class Statistics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @NotNull
     @ColumnDefault("100")
--- back/src/main/java/com/clover/recode/domain/statistics/entity/WeekReviews.java
+++ back/src/main/java/com/clover/recode/domain/statistics/entity/WeekReviews.java
@@ -17,7 +17,7 @@ public class WeekReviews {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private int count;
 
