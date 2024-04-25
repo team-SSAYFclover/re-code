512ede123e3f0c6854e9430f4da1f59eb9c93da3
배성연 <amysia@knu.ac.kr>
2024-04-25 16:12:48 +0900

    [FE] feat : 메인화면 컴포넌트 생성 2 (#13)
    - 변경 사항 1 : 복습 리스트 컴포넌트 구현 완료
    - 변경 사항 2 : 문제 추천 컴포넌트 구현 완료
    - 변경 사항 3 : 8각형 그래프 컴포넌트 구현 완료
    - 변경 사항 4 : 메인화면 컴포넌트 비율 구조 변경 -> 반응형

--- frontend/src/components/home/CloverComp.tsx
+++ frontend/src/components/home/CloverComp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CloverComp: React.FC = () => {
   return (
-    <div className="shadow-lg w-full h-fit p-5 flex-row">
+    <div className="shadow-lg w-[calc(40vh)] h-full p-5 flex-row bg-white rounded-sm">
       <div>클로버키우기</div>
     </div>
   );
