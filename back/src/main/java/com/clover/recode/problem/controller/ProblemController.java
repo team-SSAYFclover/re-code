80be120da7c45bd0ecf94c37bdd957fc4eb457c2
gyeongri <50294908+gyeongri@users.noreply.github.com>
2024-04-25 14:02:46 +0900

    [BE] feat: 문제 등록 기능 구현(#17)
    - 변경 내용 1 : ProblemController URL 매핑 슬래시 변경

--- back/src/main/java/com/clover/recode/problem/controller/ProblemController.java
+++ back/src/main/java/com/clover/recode/problem/controller/ProblemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/problems")
+@RequestMapping("/problems")
 public class ProblemController {
 
     @Autowired
