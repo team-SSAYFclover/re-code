df2deeec6fac4888744ea4a653f8b844c46b8ea9
오은아 <erropp89@naver.com>
2024-04-25 10:52:52 +0900

    [BE] design: 스웨거 설명 추가

--- back/src/main/java/com/clover/recode/domain/statistics/controller/StatisticsController.java
+++ back/src/main/java/com/clover/recode/domain/statistics/controller/StatisticsController.java
@@ -2,24 +2,27 @@ package com.clover.recode.domain.statistics.controller;
 
 import com.clover.recode.domain.statistics.service.StatisticsService;
 import com.clover.recode.global.result.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.clover.recode.global.result.ResultCode.GET_Statistics_SUCCESS;
 
 @Slf4j
 @RestController
+@Tag(name = "통계 API")
 @RequestMapping("/statistics")
 @RequiredArgsConstructor
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
+    @Operation(summary = "메인화면의 모든 통계들 정보 조회")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{userId}")
     public ResponseEntity<ResultResponse> getStatisticsList(@PathVariable("userId") int userId) {
         return ResponseEntity.ok(ResultResponse.of(GET_Statistics_SUCCESS, statisticsService.getStatisticsList(userId)));
