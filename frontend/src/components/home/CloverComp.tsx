c684959dfdf3fe838e40d01a0bba73b700812ea5
배성연 <amysia@knu.ac.kr>
2024-04-26 15:37:08 +0900

    [FE] feat : 메인화면 컴포넌트 추가 3 (#24)
    - 변경 내용 1 : 컴포넌트 위치 변경
    - 변경 내용 2 : 클로버 수정 및 애니메이션 추가
    - 변경 내용 3 : 클로버 배경 애니메이션 추가

--- frontend/src/components/home/CloverComp.tsx
+++ frontend/src/components/home/CloverComp.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import clover0 from '@/assets/clover/0.png';
+import clover1 from '@/assets/clover/1.png';
+import clover2 from '@/assets/clover/2.png';
+import clover3 from '@/assets/clover/3.png';
+import clover4 from '@/assets/clover/4.png';
+import NightSky from './cloverbgComp/NightSky';
+import style1 from '@/assets/clover/DaySky.module.css';
+import style2 from '@/assets/clover/TwilightSky.module.css';
 
-const CloverComp: React.FC = () => {
+interface CloverCompProps {
+  leafNum: number;
+}
+
+const CloverComp: React.FC<CloverCompProps> = ({ leafNum }) => {
+  const nightBG = {
+    background: 'linear-gradient(135deg, #3351B3, #1A206F, #391761)',
+  };
+  // 현재 시간 받아오기
+  const hour = new Date().getHours();
+
+  const cloverImages: { [key: number]: string } = {
+    0: clover0,
+    1: clover1,
+    2: clover2,
+    3: clover3,
+    4: clover4,
+  };
+
+  // props로 받은 leafNum에 따라 이미지 선택
+  const cloverImage = cloverImages[leafNum];
+
   return (
-    <div className="shadow-lg w-[calc(40vh)] h-full p-5 flex-row bg-white rounded-sm">
-      <div>클로버키우기</div>
+    <div className="shadow-lg h-1/3 flex justify-center relative bg-white rounded-lg overflow-hidden">
+      {/* 시간에 따라 다른 배경 컴포넌트 렌더링 */}
+      {hour >= 21 || hour < 4 ? (
+        <div className="h-full w-full absolute">
+          <div className="w-full h-3/4 absolute top-0 overflow-hidden" style={nightBG}>
+            <NightSky />
+          </div>
+          <div className="w-full h-1/4 absolute bottom-0 bg-gradient-to-t from-orange-50 to-orange-100"></div>
+        </div>
+      ) : hour >= 7 && hour < 17 ? (
+        <div className="h-full w-full absolute">
+          <div
+            className={style1.daysky}
+            style={{ width: '100%', height: '75%', position: 'absolute' }}
+          ></div>
+          <div className="w-full h-1/4 absolute bottom-0 bg-gradient-to-t from-orange-50 to-orange-100"></div>
+        </div>
+      ) : (
+        <div className="h-full w-full absolute">
+          <div
+            className={style2.twilightsky}
+            style={{ width: '100%', height: '75%', position: 'absolute' }}
+          ></div>
+          <div className="w-full h-1/4 absolute bottom-0 bg-gradient-to-t from-rose-50 to-rose-200"></div>
+        </div>
+      )}
+      <img
+        src={cloverImage}
+        alt={`clover_${leafNum}`}
+        className="z-10 animate-wiggle origin-bottom-center"
+      />
     </div>
   );
 };
--- frontend/src/components/home/TodayReviewComp.tsx
+++ frontend/src/components/home/TodayReviewComp.tsx
@@ -7,7 +7,7 @@ interface TodayReviewCompProps {
 
 const TodayReviewComp: React.FC<TodayReviewCompProps> = ({ TodayReviewData }) => {
   return (
-    <div className="shadow-lg w-full h-3/5 p-5 pb-0 flex-row bg-white rounded-sm">
+    <div className="shadow-lg w-full h-3/5 p-5 pb-0 flex flex-col bg-white rounded-sm">
       <div className="text-sm font-semibold">오늘의 복습 리스트</div>
       <div className="text-xs text-gray-500">지금까지 쌓인 복습 리스트입니다.</div>
       <div className="overflow-scroll scrollbar-hide h-4/5 pe-2 mt-2">
