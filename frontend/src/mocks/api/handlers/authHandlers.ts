c89a199fc8a88aab6259835aed5f4ed14ffe60b7
이수화 <tnghk9611@naver.com>
2024-04-29 17:18:05 +0900

    [FE] fix: api 경로 수정

--- frontend/src/mocks/api/handlers/authHandlers.ts
+++ frontend/src/mocks/api/handlers/authHandlers.ts
@@ -3,7 +3,7 @@ import { HttpResponse, http } from 'msw';
 
 export const authHandler = [
   // @TODO: 헤더 안에 토큰 추가
-  http.post('/api/auth/login', () => {
+  http.post('/auth/login', () => {
     return HttpResponse.json(postLoginRes, {
       status: 200,
       headers: {
