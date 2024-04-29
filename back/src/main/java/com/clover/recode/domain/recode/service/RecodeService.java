bd27c9cdc3678d48e9176214e6cf320bd5d94ae0
이승준 <tmdwns7809@gmail.com>
2024-04-29 17:51:18 +0900

    [BE] feat: recode api 구현중 (#26)

--- back/src/main/java/com/clover/recode/domain/recode/service/RecodeService.java
+++ back/src/main/java/com/clover/recode/domain/recode/service/RecodeService.java
@@ -2,12 +2,11 @@ package com.clover.recode.domain.recode.service;
 
 import com.clover.recode.domain.problem.dto.ProblemDto;
 import com.clover.recode.domain.recode.dto.RecodeDto;
-import com.clover.recode.domain.statistics.dto.response.StatisticsListRes;
 
 public interface RecodeService {
 
     void saveRecode(ProblemDto problemDto);
-    RecodeDto getRecode(int codeNo);
-    void addRecodeCount(int codeNo);
+    RecodeDto getRecode(int codeId);
+    void addRecodeCount(int codeId);
 
 }
