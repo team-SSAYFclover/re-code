12a59eecd5621e7d44aaea8d90599a10de289720
전찬혁 <cksgur5530@gmail.com>
2024-05-01 23:41:06 +0900

    [CE] feat: 크롬 코드 등록(#40)
    - 코드 등록 모달 디자인
    - API 호출에 사용할 Data 구성

--- extension/public/scripts/baekjoon/parse.js
+++ extension/public/scripts/baekjoon/parse.js
@@ -4,14 +4,8 @@
 function getProblemInfo(td) {
 	var problemInfo = {
 		submitNo: td[0].textContent,
-		user: td[1].textContent,
 		problemNo: td[2].textContent,
 		level: td[2].querySelector("img")?.src.split("r/")[1].replace(".svg", ""),
-		result: td[3].textContent,
-		memory: td[4].textContent,
-		time: td[5].textContent,
-		lang: td[6].querySelectorAll("a")[0].textContent,
-		byte: td[7].textContent,
 		submitTime: td[8].textContent,
 	};
 
@@ -62,49 +56,22 @@ function parseProblemDescription(doc = document) {
 
 /**
  * Readme를 생성하는 함수
- * SolvedAC API로는 level만 가져옴
- * SolvedAC API가 허용되기 전까지는 level은 html에서 파싱하도록 변경
- * SolvedAC API가 풀릴 경우를 대비해서 매개변수들은 현 상태 유지
  */
 function makeDetailMessageAndReadme(problem_info) {
 	const {
 		submitNo /* 제출 번호 */,
-		user /* 사용자 닉네임 */,
-		result /* 채점 결과 */,
-		memory /* 코드 메모리 */,
-		time /* 실행 시간 */,
-		lang /* 사용 언어 */,
-		byte /* 코드 바이트 수 */,
 		code /* 소스 코드 */,
 		level /* 문제 레벨(티어) */,
-		submitTime /* 제출 시간 */,
 		problemId /* 문제 번호 */,
 		problem_description /* 문제 설명 */,
 		problem_title /* 문제 명 */,
 		problem_input /* 문제 입력 */,
 		problem_output /* 문제 출력 */,
 		problem_tags /*문제 분류*/,
+		id,
 	} = problem_info;
 
-	const problem_tier_img =
-		level !== null && level !== undefined
-			? `<img width=\"20px\"  src=\"https://d2gd6pc034wcta.cloudfront.net/tier/${level}.svg\" class=\"solvedac-tier\">`
-			: "";
-	const bj_tier = level !== null && level !== undefined ? bj_level[level] + " " : "";
-
-	// const directory = `baekjoon/#${problemId} ${problem_title}`;
-	// const dirName = `#${problemId} ${problem_title}`;
-	// const message = `[${bj_tier}${problemId}] Title: ${problem_title}, Time: ${time} ms, Memory: ${memory} KB`;
-	// const fileName = `${convertSingleCharToDoubleChar(problem_title)}.${languages[lang]}`;
-
 	const readme =
-		// `# ${problem_tier_img} [${problem_title}](https://www.acmicpc.net/problem/${problemId}) \n\n` +
-		// `| 제출 번호 | 닉네임 | 채점 결과 | 메모리 | 시간 | 언어 | 코드 길이 |\n` +
-		// `|---|---|---|---|---|---|---|\n` +
-		// `|${submitNo}|${user}|${result.replace(
-		// 	"Upload",
-		// 	""
-		// )}|${memory}KB|${time}ms|${lang}|${byte}B|\n\n` +
 		`## 문제\n` +
 		`${problem_description}\n\n` +
 		`## 입력\n` +
@@ -113,17 +80,14 @@ function makeDetailMessageAndReadme(problem_info) {
 		`${problem_output}\n\n`;
 
 	return {
-		// directory,
-		// dirName,
-		// fileName,
-		// message,
+		id,
 		problem: {
-			no: problemId,
-			name: problem_title,
+			problemNo: problemId,
+			title: problem_title,
 			level: level,
 			content: readme,
-			tag: problem_tags,
+			tags: problem_tags,
 		},
-		code: { no: submitNo, submitTime, content: code },
+		code: { codeNo: submitNo, content: code },
 	};
 }
