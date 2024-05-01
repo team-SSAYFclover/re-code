9d079e6ef33857924cff15a752338620efe88b69
전찬혁 <cksgur5530@gmail.com>
2024-05-01 23:41:06 +0900

    [CE] feat: 크롬 코드 등록(#40)
    - 코드 등록 모달 디자인
    - API 호출에 사용할 Data 구성

--- extension/src/pages/HomePage.jsx
+++ extension/src/pages/HomePage.jsx
@@ -2,38 +2,39 @@ import { useState } from "react";
 import Alarm from "/assets/Alarm.png";
 
 const HomePage = () => {
-  const problemNum = useState(0);
+	const problemNum = useState(0);
 
-  const disConnService = () => {
-    chrome.storage.local.remove(["githubId"]);
-    window.location.reload();
-  };
+	const disConnService = () => {
+		chrome.storage.local.remove(["id"]);
+		window.location.reload();
+	};
 
-  return (
-    <>
-      <div className="flex flex-col w-full h-[150px] justify-center items-center p-5">
-        <div className="w-full grow h-1/2 flex justify-center items-start gap-4 text-[16px] ">
-          <img src={Alarm} alt="" className="w-[40px] h-[40px]" />
-          <div>
-            오늘의 복습 문제는 <span className="text-[#2CDCB2] font-bold">{problemNum}</span>개
-            입니다. <br />
-            <a
-              href="https://www.recode-d210.com"
-              className="text-[#53EDC7] underline underline-offset-4"
-            >
-              re:code로 이동하기
-            </a>
-          </div>
-        </div>
-        <button
-          className="self-end rounded-md font-bold h-8 text-[14px] bg-[#FF9292] text-MAIN2 w-20"
-          onClick={disConnService}
-        >
-          연동 해제
-        </button>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex flex-col w-full h-[150px] justify-center items-center p-5">
+				<div className="w-full grow h-1/2 flex justify-center items-start gap-4 text-[16px] ">
+					<img src={Alarm} alt="" className="w-[40px] h-[40px]" />
+					<div>
+						오늘의 복습 문제는{" "}
+						<span className="text-[#2CDCB2] font-bold">{problemNum}</span>개 입니다.{" "}
+						<br />
+						<a
+							href="https://www.recode-d210.com"
+							className="text-[#53EDC7] underline underline-offset-4"
+						>
+							re:code로 이동하기
+						</a>
+					</div>
+				</div>
+				<button
+					className="self-end rounded-md font-bold h-8 text-[14px] border-[1px] border-[#FF9292] bg-[#FF9292] text-white w-20 hover:bg-white hover:text-[#FF9292]"
+					onClick={disConnService}
+				>
+					연동 해제
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default HomePage;
