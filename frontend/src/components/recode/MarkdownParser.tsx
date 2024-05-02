0c74b51b8fe7f02268806ab618b4f414d05e5e74
suhwa <tnghk9611@naver.com>
2024-05-03 03:47:14 +0900

    [FE] design: 스타일 수정

--- frontend/src/components/recode/MarkdownParser.tsx
+++ frontend/src/components/recode/MarkdownParser.tsx
@@ -3,7 +3,11 @@ import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
 
 const MarkdownParser = ({ markdown }: { markdown: string }) => {
-  return <Markdown rehypePlugins={[rehypeRaw, rehypeSanitize]}>{markdown}</Markdown>;
+  return (
+    <Markdown rehypePlugins={[rehypeRaw, rehypeSanitize]} className="prose max-w-[1920px]">
+      {markdown}
+    </Markdown>
+  );
 };
 
 export default MarkdownParser;
