8c017040f597dce0887ad2273e1ed9977ef4553f
배성연 <amysia@knu.ac.kr>
2024-05-02 14:17:55 +0900

    [FE] feat: 문제 조회 페이지 문제 컴포넌트 구현 (#37)
    - 변경 내용 1 : 티어 메달 asset 추가
    - 변경 내용 2 : 티어 메달 ProblemComp에서 동적 import 구현
    - 변경 내용 3 : ProblemComp 내 알고리즘 카테고리 표시 시 key 지정,
      overflow 지정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   base: '/',
 });
