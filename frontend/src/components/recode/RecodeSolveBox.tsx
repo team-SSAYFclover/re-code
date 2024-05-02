858b923c162e26614011d52c6aa42e414e8faa0b
이수화 <tnghk9611@naver.com>
2024-05-02 17:56:18 +0900

    [FE] feat: 문제 풀기 페이지 UI 구현 (#25)

---
+++ frontend/src/components/recode/RecodeSolveBox.tsx
@@ -0,0 +1,10 @@
+import Markdown from 'react-markdown';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeRaw from 'rehype-raw';
+import rehypeSanitize from 'rehype-sanitize';
+
+const RecodeSolveBox = ({ recode, answer }: { recode: string; answer: string[] }) => {
+  return <Markdown rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeHighlight]}>{recode}</Markdown>;
+};
+
+export default RecodeSolveBox;
