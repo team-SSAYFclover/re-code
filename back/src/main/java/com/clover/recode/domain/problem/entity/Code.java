b4509f4f83227eb422ea0ae246c7d0a7dc4ed870
전찬혁 <cksgur5530@gmail.com>
2024-05-02 10:20:40 +0900

    [BE] feat: Code, Problem Entity (#42)

--- back/src/main/java/com/clover/recode/domain/problem/entity/Code.java
+++ back/src/main/java/com/clover/recode/domain/problem/entity/Code.java
@@ -1,31 +1,66 @@
 package com.clover.recode.domain.problem.entity;
 
+import com.clover.recode.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-    // Code Entity
+// Code Entity
     @Entity
     @Getter
     @Setter
     @NoArgsConstructor
-    @Table(name = "code")
+    @EntityListeners(value = AuditingEntityListener.class)
+    @ToString(of = {"id", "codeNo", "name", "content"})
     public class Code {
         @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
-        private Long userId;
-        private Long problemId;
+
+        @JoinColumn(name = "user_id")
+        @ManyToOne(fetch = FetchType.LAZY)
+        private User user;
+
+        @JoinColumn(name = "problem_id")
+        @ManyToOne(fetch = FetchType.LAZY)
+        private Problem problem;
+
+        @Column(unique = true)
         private Long codeNo;
-        //private LocalDateTime submitTime;
+
+        @Column(nullable = false)
         private String name;
 
-
-        @Column(columnDefinition = "TEXT")
+        @Column(nullable = false, columnDefinition = "LONGTEXT")
         private String content;
-        private Boolean deleted;
+
+        @ColumnDefault("false")
+        private boolean deleted;
+
+        @ColumnDefault("true")
+        private boolean reviewStatus;
+
+        @CreatedDate
+        @Column(updatable = false)
         private LocalDateTime createdTime;
-        private Boolean reviewStatus;
+        private LocalDateTime updatedTime;
+
+        @PrePersist
+        public void prePersist() {
+            LocalDateTime now = LocalDateTime.now();
+            createdTime = now;
+            updatedTime = now;
+        }
+
+        @PreUpdate
+        public void preUpdate() {
+            updatedTime = LocalDateTime.now();
+        }
     }
