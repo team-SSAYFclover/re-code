3e0431e87f1934c566f58a123c72f3fa309c857a
전찬혁 <cksgur5530@gmail.com>
2024-05-03 10:22:05 +0900

    [BE] feat: Set-Cookie 노출 헤더 추가

--- back/src/main/java/com/clover/recode/global/config/SecurityConfig.java
+++ back/src/main/java/com/clover/recode/global/config/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
               configuration.setMaxAge(3600L);
 
               // 브라우저에 노출할 헤더 설정
-              configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+              configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie"));
               return configuration;
             }));
 
