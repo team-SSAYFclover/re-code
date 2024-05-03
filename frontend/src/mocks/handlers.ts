1fe4776400eab1ecf0b69f3c43808b35ccbf5811
이수화 <tnghk9611@naver.com>
2024-05-03 16:47:18 +0900

    [FE] feat: user 정보 조회 msw 코드 작성 (#50)

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { alarmHandler } from '@/mocks/api/handlers/alarmHandlers';
 import { authHandler } from '@/mocks/api/handlers/authHandlers';
+import { userHandlers } from './api/handlers/userHandlers';
 
-export const handlers = [...alarmHandler, ...authHandler];
+export const handlers = [...alarmHandler, ...authHandler, ...userHandlers];
