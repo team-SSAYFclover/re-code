6ddfce9d8c59090c2cf63dc1a44b8a0851633628
suhwa <tnghk9611@naver.com>
2024-05-05 01:11:43 +0900

    [FE] feat: 오늘의 복습 문제 리스트 조회 API 통신 구현 (#51)

--- frontend/src/pages/recode/RecodePage.tsx
+++ frontend/src/pages/recode/RecodePage.tsx
@@ -1,27 +1,32 @@
 import clover from '@/assets/clover.png';
+import Progressbar from '@/components/@common/Progressbar';
 import RecodeList from '@/components/recode/RecodeList';
-
-export interface IRecodeItemProps {
-  problemNo: number;
-  problemTitle: string;
-  count: number;
-  solvedYn: boolean;
-}
+import { useRecode } from '@/hooks/recode/useRecode';
+import { useNavigate } from 'react-router-dom';
 
 const RecodePage = () => {
-  const TodayReviewData: IRecodeItemProps[] = [
-    { problemNo: 3302, problemTitle: 'Job Scheduling', count: 2, solvedYn: true },
-    { problemNo: 11779, problemTitle: '최소비용 구하기 2', count: 1, solvedYn: false },
-    { problemNo: 1486, problemTitle: '등산', count: 1, solvedYn: true },
-    { problemNo: 1261, problemTitle: '알고스팟', count: 2, solvedYn: false },
-    { problemNo: 11559, problemTitle: 'Puyo Puyo', count: 2, solvedYn: false },
-    { problemNo: 1197, problemTitle: '최소 스패닝 트리', count: 3, solvedYn: false },
-    { problemNo: 17471, problemTitle: '게리맨더링', count: 1, solvedYn: false },
-  ];
+  const navigate = useNavigate();
+  const { useGetTodayRecodeList } = useRecode();
+  const { data, isLoading } = useGetTodayRecodeList();
+
+  if (isLoading) {
+    return <div>로딩 중</div>;
+  }
+
+  if (!data) {
+    return <div>데이터 없음</div>;
+  }
 
   const solvedCnt = () => {
-    return TodayReviewData.filter((x) => x.solvedYn).length;
+    return data.filter((x) => x.completed).length;
   };
+
+  const handleStartSolve = () => {
+    const startCodeId = data.filter((x) => !x.completed)[0].codeId;
+    navigate(`/recode/${startCodeId}`);
+  };
+
+  const percentage: number = (solvedCnt() / data.length) * 100;
 
   return (
     <div className="w-full h-full flex justify-center items-center bg-gray-100/50 relative">
@@ -36,14 +41,26 @@ const RecodePage = () => {
               지금까지 쌓인 복습 리스트입니다. 문제를 눌러 복습을 시작해보아요.
             </div>
           </div>
-          <div className="w-[50px] text-xl">
-            {solvedCnt()} / {TodayReviewData.length}
+          <div className="w-[180px] text-right">
+            <div className=" pr-1 text-md text-right text-gray-500">
+              <span className="text-MAIN1">{data.length}</span>문제 중&nbsp;
+              <span className="text-MAIN1">{solvedCnt()}</span>문제 복습 완료
+            </div>
+            <div className="flex justify-center items-center">
+              <Progressbar percentage={percentage} height="h-2" roundWidth="w-2" />
+              <span className="text-sm text-[#51A1FF]">&nbsp;{Math.floor(percentage)}%&nbsp;</span>
+            </div>
           </div>
         </div>
-        <RecodeList review={TodayReviewData} />
+        <div className="h-[calc(100%-160px)]">
+          <RecodeList review={data} />
+        </div>
         <div className="h-20 flex justify-end items-center">
-          <button className="h-10 bg-MAIN1 text-MAIN2 px-3 rounded-md hover:bg-[#2CD8AE]">
-            처음부터 복습하기
+          <button
+            className="h-10 bg-MAIN1 text-MAIN2 px-3 rounded-md hover:bg-[#2CD8AE]"
+            onClick={handleStartSolve}
+          >
+            복습 시작하기
           </button>
         </div>
       </div>
