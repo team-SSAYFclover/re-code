692081eea05fcbb9e0acf0deda298b6e7efbb8cc
오은아 <erropp89@naver.com>
2024-05-05 23:04:50 +0900

    [BE] feat: ERD수정에 따른 연관관계 및 식별관계 수정(#22)

--- back/src/main/java/com/clover/recode/domain/statistics/entity/AlgoReview.java
+++ back/src/main/java/com/clover/recode/domain/statistics/entity/AlgoReview.java
@@ -3,6 +3,8 @@ package com.clover.recode.domain.statistics.entity;
 import com.clover.recode.domain.problem.entity.Tag;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Getter
 @Setter
@@ -11,21 +13,43 @@ import lombok.*;
 @NoArgsConstructor
 @Entity
 @ToString
+@DynamicInsert
 public class AlgoReview {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Integer count;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
     @JoinColumn(name = "statistics_id")
     private Statistics statistics;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tag_id")
-    private Tag tag;
+    @ColumnDefault("0")
+    private Integer count1;
+
+    @ColumnDefault("0")
+    private Integer count2;
+
+    @ColumnDefault("0")
+    private Integer count3;
+
+    @ColumnDefault("0")
+    private Integer count4;
+
+    @ColumnDefault("0")
+    private Integer count5;
+
+    @ColumnDefault("0")
+    private Integer count6;
+
+    @ColumnDefault("0")
+    private Integer count7;
+
+    @ColumnDefault("0")
+    private Integer count8;
+
 
 
 }
--- back/src/main/java/com/clover/recode/domain/statistics/entity/Statistics.java
+++ back/src/main/java/com/clover/recode/domain/statistics/entity/Statistics.java
@@ -23,6 +23,7 @@ public class Statistics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "statistics_id")
     private Long id;
 
     @NotNull
