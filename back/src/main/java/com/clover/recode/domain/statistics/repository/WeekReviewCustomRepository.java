3c910c39ca5fe0ee152bd903e55b02be62819e6a
오은아 <erropp89@naver.com>
2024-05-07 17:58:40 +0900

    [BE] feat:복습문제 이름 중복 시 뒤에 (n)을 붙여줌 (#54)

--- back/src/main/java/com/clover/recode/domain/statistics/repository/WeekReviewCustomRepository.java
+++ back/src/main/java/com/clover/recode/domain/statistics/repository/WeekReviewCustomRepository.java
@@ -1,7 +1,5 @@
 package com.clover.recode.domain.statistics.repository;
 
-import com.clover.recode.domain.statistics.dto.WeekReviewDto;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -10,7 +8,7 @@ public interface WeekReviewCustomRepository {
 
     List<Integer> findReviewsBetweenDates(LocalDate startOfWeek, LocalDate endOfWeek, Long statisticsId);
 
-    Integer countByTodayWeview(Long statisticsId, LocalDate today);
+    Integer countByTodayReview(Long statisticsId, LocalDate today);
 
 
 }
