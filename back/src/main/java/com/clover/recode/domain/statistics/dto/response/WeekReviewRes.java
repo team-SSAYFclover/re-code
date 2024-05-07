6f525b917ef46f86d60160d1c67afc368dd50603
오은아 <erropp89@naver.com>
2024-05-07 10:02:08 +0900

    [BE] feat: WeekReviewRes 생성(#53)

---
+++ back/src/main/java/com/clover/recode/domain/statistics/dto/response/WeekReviewRes.java
@@ -0,0 +1,19 @@
+package com.clover.recode.domain.statistics.dto.response;
+
+public class WeekReviewRes {
+
+    int mon;
+
+    int tue;
+
+    int wed;
+
+    int thu;
+
+    int fri;
+
+    int sat;
+
+    int sun;
+
+}
