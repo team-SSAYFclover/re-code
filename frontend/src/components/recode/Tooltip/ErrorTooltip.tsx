205ebc437ec426d50dd2abd288434e4f1c280215
이수화 <tnghk9611@naver.com>
2024-05-07 17:50:25 +0900

    [FE] feat: 성공 및 실패 시 나오는 툴팁 구현 (#49)

---
+++ frontend/src/components/recode/Tooltip/ErrorTooltip.tsx
@@ -0,0 +1,41 @@
+const ErrorTooltip = ({ input, answer }: { input: string; answer: string }) => {
+  if (input.length < answer.length) {
+    return (
+      <div className="p-2 bg-white">
+        <div className="text-[#FF0404] py-1">
+          정답 {answer.length}자 중 {input.length}자를 입력하셨네요. 다시 입력해볼까요?
+        </div>
+        <div className="h-10 text-lg bg-[#FFF3F3] flex justify-center items-center">
+          {input.split('').map((x, idx) => {
+            return (
+              <span
+                key={idx}
+                className={`${answer[idx] === x ? 'text-[#6A6A6A]' : 'text-red-600'}`}
+              >
+                {x}
+              </span>
+            );
+          })}
+          <span>{Array.from({ length: answer.length - input.length }, () => ' _ ').join(' ')}</span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-2 bg-white">
+      <div className="text-[#FF0404] py-1">아쉽게 틀리셨네요. 다시 입력해볼까요?</div>
+      <div className="h-10 text-lg bg-[#FFF3F3] flex justify-center items-center">
+        {input.split('').map((x, idx) => {
+          return (
+            <span key={idx} className={`${answer[idx] === x ? 'text-[#6A6A6A]' : 'text-red-600'}`}>
+              {x}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ErrorTooltip;
