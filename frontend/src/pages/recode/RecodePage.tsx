eddb7cd211d88b930af40bc415ebd4d2b9c598d0
이수화 <tnghk9611@naver.com>
2024-05-09 17:09:48 +0900

    [FE] feat: 오늘의 복습 리스트 스켈레톤 및 컨텐츠 없을때 ui 적용 (#65)

--- frontend/src/pages/recode/RecodePage.tsx
+++ frontend/src/pages/recode/RecodePage.tsx
@@ -1,32 +1,36 @@
 import clover from '@/assets/clover.png';
+import NoContent from '@/assets/lotties/noContent.json';
 import Progressbar from '@/components/@common/Progressbar';
+import Nocontent from '@/components/recode/Nocontent';
 import RecodeList from '@/components/recode/RecodeList';
+import RecodeSkeleton from '@/components/recode/RecodeSkeleton';
 import { useRecode } from '@/hooks/recode/useRecode';
+import Lottie from 'react-lottie';
 import { useNavigate } from 'react-router-dom';
 
 const RecodePage = () => {
+  const defaltOptions = {
+    loop: true,
+    autoPlay: true,
+    animationData: NoContent,
+  };
+
   const navigate = useNavigate();
   const { useGetTodayRecodeList } = useRecode();
   const { data, isLoading } = useGetTodayRecodeList();
 
-  if (isLoading) {
-    return <div>로딩 중</div>;
-  }
-
-  if (!data) {
-    return <div>데이터 없음</div>;
-  }
-
   const solvedCnt = () => {
-    return data.filter((x) => x.completed).length;
+    return data ? data.filter((x) => x.completed).length : 0;
   };
 
   const handleStartSolve = () => {
-    const startCodeId = data.filter((x) => !x.completed)[0].codeId;
+    const startCodeId = data ? data.filter((x) => !x.completed)[0].codeId : undefined;
     navigate(`/recode/${startCodeId}`);
   };
 
-  const percentage: number = (solvedCnt() / data.length) * 100;
+  const percentage: number = data ? (solvedCnt() / data.length) * 100 : 0;
+
+  console.log('받아온 데이터', data);
 
   return (
     <div className="w-full h-full flex justify-center items-center bg-gray-100/50 relative">
@@ -42,27 +46,68 @@ const RecodePage = () => {
             </div>
           </div>
           <div className="w-[180px] text-right">
-            <div className=" pr-1 text-md text-right text-gray-500">
-              <span className="text-MAIN1">{data.length}</span>문제 중&nbsp;
-              <span className="text-MAIN1">{solvedCnt()}</span>문제 복습 완료
-            </div>
-            <div className="flex justify-center items-center">
-              <Progressbar percentage={percentage} height="h-2" roundWidth="w-2" />
-              <span className="text-sm text-[#51A1FF]">&nbsp;{Math.floor(percentage)}%&nbsp;</span>
-            </div>
+            {data && data.length > 0 && (
+              <>
+                <div className=" pr-1 text-md text-right text-gray-500">
+                  <span className="text-MAIN1">{data?.length}</span>문제 중&nbsp;
+                  <span className="text-MAIN1">{solvedCnt()}</span>문제 복습 완료
+                </div>
+                <div className="flex justify-center items-center">
+                  <Progressbar percentage={percentage} height="h-2" roundWidth="w-2" />
+                  <span className="text-sm text-[#51A1FF]">
+                    &nbsp;{Math.floor(percentage)}%&nbsp;
+                  </span>
+                </div>
+              </>
+            )}
           </div>
         </div>
         <div className="h-[calc(100%-160px)]">
-          <RecodeList review={data} />
+          {isLoading ? (
+            <>
+              <RecodeSkeleton />
+              <RecodeSkeleton />
+              <RecodeSkeleton />
+              <RecodeSkeleton />
+            </>
+          ) : data && data.length > 0 ? (
+            <RecodeList review={data} />
+          ) : (
+            <div className="bg-white rounded-lg w-full h-full ">
+              <Nocontent
+                text={
+                  <>
+                    <div className="text-lg">
+                      오늘의 복습문제가 없어요 <br />
+                      문제를 풀어 복습 문제를 추가해볼까요?
+                    </div>
+                    <button
+                      className="border text-sm border-MAIN1 text-MAIN1 px-4 py-2 mt-2 rounded-md hover:text-white hover:bg-MAIN1"
+                      onClick={() =>
+                        window.open(
+                          `https://www.acmicpc.net/problem/${Math.floor(Math.random() * 10000 + 1000)}`
+                        )
+                      }
+                    >
+                      오늘의 랜덤 문제 풀기
+                    </button>
+                  </>
+                }
+                icon={<Lottie options={defaltOptions} width={160} height={160} />}
+              />
+            </div>
+          )}
         </div>
-        <div className="h-20 flex justify-end items-center">
-          <button
-            className="h-10 bg-MAIN1 text-MAIN2 px-3 rounded-md hover:bg-[#2CD8AE]"
-            onClick={handleStartSolve}
-          >
-            복습 시작하기
-          </button>
-        </div>
+        {!isLoading && data && data.length > 0 && (
+          <div className="h-20 flex justify-end items-center">
+            <button
+              className="h-10 bg-MAIN1 text-MAIN2 px-3 rounded-md hover:bg-[#2CD8AE]"
+              onClick={handleStartSolve}
+            >
+              복습 시작하기
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
