bdac92de52a70783b1f60ff2951bc5d889efc3de
이승준 <tmdwns7809@gmail.com>
2024-05-10 11:30:11 +0900

    [BE] fix: recode 반환 수정

--- back/src/main/java/com/clover/recode/domain/recode/dto/RecodeRes.java
+++ back/src/main/java/com/clover/recode/domain/recode/dto/RecodeRes.java
@@ -1,6 +1,9 @@
 package com.clover.recode.domain.recode.dto;
 
+<<<<<<< Updated upstream
 import com.clover.recode.domain.problem.dto.ProblemRegistReq;
+=======
+>>>>>>> Stashed changes
 import lombok.*;
 
 import java.util.List;
@@ -9,7 +12,11 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class RecodeRes {
+<<<<<<< Updated upstream
     private ProblemRegistReq problemRegistReq;
+=======
+    private Problem problem;
+>>>>>>> Stashed changes
     private String recode;
     private List<String> answers;
 }
