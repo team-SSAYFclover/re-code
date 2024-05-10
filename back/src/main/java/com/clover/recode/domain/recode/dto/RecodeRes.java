34670aa74ad2607cb9cfe13c4eb7bf533c45b8ef
이승준 <tmdwns7809@gmail.com>
2024-05-10 13:28:25 +0900

    [BE] fix: recode 반환 객체 수정 (#75)

--- back/src/main/java/com/clover/recode/domain/recode/dto/RecodeRes.java
+++ back/src/main/java/com/clover/recode/domain/recode/dto/RecodeRes.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class RecodeRes {
-    private Problem problem;
+    private ProblemDto problem;
     private String recode;
     private List<String> answers;
 }
