ff57062fb2134ec3fa6679ee8eb608b7db5021ad
이승준 <tmdwns7809@gmail.com>
2024-05-10 11:30:11 +0900

    [BE] fix: recode 반환 수정

--- back/src/main/java/com/clover/recode/domain/recode/service/RecodeServiceImpl.java
+++ back/src/main/java/com/clover/recode/domain/recode/service/RecodeServiceImpl.java
@@ -154,6 +154,7 @@ public class RecodeServiceImpl implements RecodeService {
         List<Tag> tags = problem.getTags();
         for (Tag tag : tags) tagNames.add(tag.getName());
 
+<<<<<<< Updated upstream
         return new RecodeRes(ProblemRegistReq.builder()
                 .problemNo(problem.getProblemNo())
                 .title(problem.getTitle())
@@ -161,6 +162,15 @@ public class RecodeServiceImpl implements RecodeService {
                 .content(problem.getContent())
                 .tags(tagNames)
                 .build()
+=======
+        return new RecodeRes(com.clover.recode.domain.recode.dto.Problem.builder()
+                    .problemNo(problem.getProblemNo())
+                    .title(problem.getTitle())
+                    .level(problem.getLevel())
+                    .content(problem.getContent())
+                    .tags(tagNames)
+                    .build()
+>>>>>>> Stashed changes
                 , sb.toString()
                 , answers);
     }
