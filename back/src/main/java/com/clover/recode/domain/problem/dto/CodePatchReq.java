c716d986c9db78068bf0c535e58365017d3ba44d
전찬혁 <cksgur5530@gmail.com>
2024-05-10 17:04:49 +0900

    [BE] feat: code API 구현 (#69)

---
+++ back/src/main/java/com/clover/recode/domain/problem/dto/CodePatchReq.java
@@ -0,0 +1,13 @@
+package com.clover.recode.domain.problem.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CodePatchReq {
+
+  private String name;
+  private Boolean reviewStatus;
+
+}
