0a8ed99d2276473c11746d80bcc1dc921e46dc3d
sj <tmdwns7809@gmail.com>
2024-05-15 16:22:15 +0900

    [BE] fix: recode 다른 언어 생성되지 않는 버그 수정

--- back/src/test/java/com/clover/recode/domain/recode/service/RecodeServiceImplTest.java
+++ back/src/test/java/com/clover/recode/domain/recode/service/RecodeServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.clover.recode.domain.recode.service;
 
 import com.clover.recode.domain.recode.dto.code.ExampleCode1;
+import com.clover.recode.domain.recode.dto.code.ExampleCode3;
+import com.clover.recode.global.result.error.ErrorCode;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.clover.recode.domain.problem.entity.Code;
 import com.clover.recode.domain.problem.repository.CodeRepository;
@@ -8,38 +10,27 @@ import com.clover.recode.global.result.error.exception.BusinessException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static com.clover.recode.global.result.error.ErrorCode.CODE_NOT_EXISTS;
+
 @SpringBootTest
 class RecodeServiceImplTest {
 
 //    @Autowired
 //    RecodeServiceImpl service;
-
+//
 //    @Autowired
 //    CodeRepository codeRepository;
-
-//    @Test
-//    void GPTTest() {
-//        String recode = service.getRecode(EnglishPrompt.prompt);
-//    }
-
+//
 //    @Test
 //    void createRecodeTest() {
-//        service.createRecode(ExampleCode1.code);
+//        service.createRecode(ExampleCode3.code);
 //    }
 
 //    @Test
 //    void getRecodeTest() {
-//        Code code = codeRepository.findById(12L)
-//                .orElseThrow(() -> new BusinessException(USER_NOT_EXISTS));
+//        Code code = codeRepository.findById(18L)
+//                .orElseThrow(() -> new BusinessException(CODE_NOT_EXISTS));
 //
 //        service.getRecodeFromCode(code);
-//    }
-//
-//    @Test
-//    void recodeSubmitTest() {
-//        Code code = codeRepository.findById(1L)
-//                .orElseThrow(() -> new BusinessException(USER_NOT_EXISTS));
-//
-//        service.recodeSubmit(code.getId());
 //    }
 }
