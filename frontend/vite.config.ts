23b019052ba30946c465219e986d7713a441f000
이수화 <tnghk9611@naver.com>
2024-05-17 17:54:13 +0900

    [FE] fix: million js 임시 제거

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,10 @@ import svgr from '@svgr/rollup';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react(), svgr({})],
+  plugins: [react(), svgr({})],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
