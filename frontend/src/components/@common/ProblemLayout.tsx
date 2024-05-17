571e409fff7ea8b108de641200c71b8206d3cf80
이수화 <tnghk9611@naver.com>
2024-05-17 17:15:57 +0900

    [FE] feat: 전체 레이아웃 수정

--- frontend/src/components/@common/ProblemLayout.tsx
+++ frontend/src/components/@common/ProblemLayout.tsx
@@ -4,11 +4,11 @@ import { Outlet } from 'react-router-dom';
 
 const ProblemLayout = () => {
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-screen h-screen flex  min-w-[1200px]">
       <Navbar />
       <div className="w-full h-full pl-60">
         <Header />
-        <main className="h-[calc(100vh-64px)] w-full px-20">
+        <main className="h-[calc(100vh-64px)] w-full">
           <Outlet />
         </main>
       </div>
--- frontend/src/pages/problem/ProblemDetailPage.tsx
+++ frontend/src/pages/problem/ProblemDetailPage.tsx
@@ -1,11 +1,7 @@
 import ProblemDetailContent from '@/components/problem/ProblemDetailContent';
 
 const ProblemDetailPage = () => {
-  return (
-    <div className="h-[calc(90vh)]">
-      <ProblemDetailContent />
-    </div>
-  );
+  return <ProblemDetailContent />;
 };
 
 export default ProblemDetailPage;
